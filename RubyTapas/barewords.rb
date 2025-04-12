751316386271ad2ca65047b60c382d64b487c6ab
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-11 20:13:02 -0500

    process 1 - Please the trolls! Changed the greeting code even though the actual logic never changed.
    BREAKDOWN FOR FUTURE ME
    
    - title remained method argument
    - full name was broken out into two arguments
    - designation became the property of a single instance
    - special feature became a member of a composite object.
    - salutation became the property of a class
    - progname version became a constant in a program wide module name space
    - progname name became global

--- RubyTapas/barewords.rb
+++ RubyTapas/barewords.rb
@@ -1,23 +1,31 @@
-class ObsequiousImp
-  def greet(title, full_name)
-    salutation = "Most agreeable to see you"
-    progname = "Dis-organizer"
-    version = "Mark 7"
-    designation = "Seeree"
-    service_inquiry = "order you a coffee"
-  
-  
-  
-  
-  
-  
-  
+$progname = "Dis-organizer"
 
-    puts "#{salutation}, #{title} #{full_name}. ",
-          "Welcome to #{progname} version #{version}.",
-          "My name is #{designation}.",
-          "May I #{service_inquiry}?"
+module DisOrganizer
+  VERSION = "Mark 7"
+end
+
+class CoffeeEnabled
+  def service_inquiry
+    "order you a coffee"
+  end
+end
+class ObsequiousImp
+  SALUTATION = "Most agreeable to see you"
+
+  attr_accessor :special_feature
+
+  def initialize(designation)
+    @designation = designation
+  end
+  def greet(title, first_name, last_name)
+  
+    puts "#{SALUTATION}, #{title} #{first_name} #{last_name}. ",
+          "Welcome to #{$progname} version #{DisOrganizer::VERSION}.",
+          "My name is #{@designation}.",
+          "May I #{special_feature.service_inquiry}?"
   end
 end
 
-ObsequiousImp.new.greet "Commander", "Sam Vimes"
+imp = ObsequiousImp.new("Seeree")
+imp.special_feature = CoffeeEnabled.new
+imp.greet "Commander", "Sam", "Vimes"
