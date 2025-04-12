0782d11e03938068046534a0fa1e74ac3667a41b
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-11 19:52:22 -0500

    Barewords - original greeting

---
+++ RubyTapas/barewords.rb
@@ -0,0 +1,23 @@
+class ObsequiousImp
+  def greet(title, full_name)
+    salutation = "Most agreeable to see you"
+    progname = "Dis-organizer"
+    version = "Mark 7"
+    designation = "Seeree"
+    service_inquiry = "order you a coffee"
+  
+  
+  
+  
+  
+  
+  
+
+    puts "#{salutation}, #{title} #{full_name}. ",
+          "Welcome to #{progname} version #{version}.",
+          "My name is #{designation}.",
+          "May I #{service_inquiry}?"
+  end
+end
+
+ObsequiousImp.new.greet "Commander", "Sam Vimes"
