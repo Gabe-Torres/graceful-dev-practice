bca13928299a7cb11248c177582260c0e0e846e8
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-13 22:28:29 -0500

    match is a regular expression method that can be used with blocks to make things easier

--- RubyTapas/match.rb
+++ RubyTapas/match.rb
@@ -12,10 +12,14 @@
 # end
 # 
 # In the case of needing a success or fail then a traditional if statement is best
-# but why do match pass through the block value?
-r = /\d{3}-\d{4}/
-if match = r.match("my number is 555-1234")
-  puts "Found #{match}"
-else 
-  raise "no number found"
-end
+# but why do match pass through the block return value?
+# r = /\d{3}-\d{4}/
+# if match = r.match("my number is 555-1234")
+#   puts "Found #{match}"
+# else 
+#   raise "no number found"
+# end
+# 
+r = /(\d{3})-\d{4}/
+exchange =  r.match("number is 555-1234") { |match| match[1] }
+puts exchange
