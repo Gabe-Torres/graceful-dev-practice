d9dfd41d1f3c442153d368dcbf0d84f74dbcdc17
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-13 22:22:17 -0500

    another couple methods using match on with the regular expression

--- RubyTapas/match.rb
+++ RubyTapas/match.rb
@@ -1,6 +1,21 @@
 # Using regular expression to match phone numbers to local format.
 # This method works, wrapping single statement is not not needed but lets others know that '=' within the conditional was purposeful.
+# r = /\d{3}-\d{4}/
+# if (match = r.match("my number is 555-1234"))
+#   puts "Found #{match}"
+# end
+# 
+# This method can replace the 'if' statement but doesn't consider 'what if the match fails' aka nil.
+# r = /\d{3}-\d{4}/
+# r.match("my number is 555-1234") do |match|
+#   puts "Found #{match}"
+# end
+# 
+# In the case of needing a success or fail then a traditional if statement is best
+# but why do match pass through the block value?
 r = /\d{3}-\d{4}/
-if (match = r.match("my number is 555-1234"))
+if match = r.match("my number is 555-1234")
   puts "Found #{match}"
+else 
+  raise "no number found"
 end
