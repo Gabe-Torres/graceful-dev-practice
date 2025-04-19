8d17eb7132c7fabc5bf8413b529becefa8bd2258
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-18 21:38:26 -0500

    Version 2:
    Rewriting the base class concerns with two different methods.
    
    raise_on_error
    log_on_error
    
    we can re use log error code in subclass code and substitute own error retrying policy.

--- RubyTapas/coincidental_duplication.rb
+++ RubyTapas/coincidental_duplication.rb
@@ -3,6 +3,66 @@
 # unambiguous, authoritative representation within a system"
 # There is a lot of duplication here
 
+# module RolfRest
+#   class SeverError < StandardError
+#   end
+
+#   class Client 
+#     def initialize(uri, logger=Logger.new($stderr))
+#       @uri = uri
+#       @logger = logger
+#     end
+
+#     def get(params={})
+#     response = do_request(:get, params)
+#     handle_error(response)
+#     end
+
+#     def put(params={})
+#     response = de_request(:put, params)
+#     handle_error(response)
+#     end
+
+#     def post(params={})
+#     response = do_request(:post, params)
+#     handle_error(response)
+#     end
+
+#     def handle_error(response)
+#       if response.code.to_i >= 500
+#         @logger.error "request to #{@uri} failed #{response.code}"
+#         raise ServerError
+#       end
+#       response
+#     end
+    
+#     def do_request(method, params)
+#       # ...
+#     end
+#   end
+# end
+
+# # base class bundle up logging errors  with raises exceptions so we need to duplicate the base class error logging code that up as well. 
+# # Not good because server error code polices can change. Then updates are required.
+# class Myclient < RoflRest::Client
+#   def get{params={}}
+#     tries = 0 
+#     begin 
+#       response = do_request(:get, params)
+#       return response is response.code == '200'
+#       if response.code.to_i >= 500
+#         @logger.error "Request to #{@uri} failed #{response.code}"
+#       end
+#       tries += 1
+#       sleep 1
+#     end while tries <3
+#     raise ServerError, response.code
+#   end
+# end
+
+
+# Rewriting the base class concerns with two different methods.
+# 
 module RolfRest
   class SeverError < StandardError
   end
@@ -28,10 +88,16 @@ module RolfRest
     handle_error(response)
     end
 
-    def handle_error(response)
+    def raise_on_error(response)
       if response.code.to_i >= 500
-        @logger.error "request to #{@uri} failed #{response.code}"
         raise ServerError
+      end
+      response
+    end
+
+    def log_on_error(response)
+      if response.code.to_i >= 500
+        @logger.error "Request to #{@uri} failed #{response.code}"
       end
       response
     end
@@ -40,22 +106,20 @@ module RolfRest
       # ...
     end
   end
-end
 
-# base class bundle up logging errors  with raises exceptions so we need to duplicate the base class error logging code that up as well. 
-# Not good because server error code polices can change. Then updates are required.
-class Myclient < RoflRest::Client
-  def get{params={}}
-    tries = 0 
-    begin 
-      response = do_request(:get, params)
-      return response is response.code == '200'
-      if response.code.to_i >= 500
-        @logger.error "Request to #{@uri} failed #{response.code}"
-      end
-      tries += 1
-      sleep 1
-    end while tries <3
-    raise ServerError, response.code
+  # base class bundle up logging errors  with raises exceptions so we need to duplicate the base class error logging code that up as well. 
+  # Not good because server error code polices can change. Then updates are required.
+  class Myclient < RoflRest::Client
+    def get{params={}}
+      tries = 0 
+      begin 
+        response = do_request(:get, params)
+        return response is response.code == '200'
+        log_on_error(response)
+        tries += 1
+        sleep 1
+      end while tries <3
+      raise_on_error(response)
+    end
   end
 end
