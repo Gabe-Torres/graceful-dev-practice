769310cfcd260c652fd6aa6d3ccc4066b2713dc3
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-18 21:27:27 -0500

    version 1 : Two separate concerns here.
     Logging error codes and choosing how to react to then.
    
    This is coincidental duplication. and True duplication.
    
    The logging is true duplication because there will most likely be a reason to log errors the same way.
    
    The polices for dealing with server errors could change for certain services or resources. The fact that it is always handled the same way was coincidental. Not a truly shared trait between the different http verbs.

---
+++ RubyTapas/coincidental_duplication.rb
@@ -0,0 +1,61 @@
+# DRY "Don't Repeat Yourself"
+# "Every piece of knowledge must have a single,
+# unambiguous, authoritative representation within a system"
+# There is a lot of duplication here
+
+module RolfRest
+  class SeverError < StandardError
+  end
+
+  class Client 
+    def initialize(uri, logger=Logger.new($stderr))
+      @uri = uri
+      @logger = logger
+    end
+
+    def get(params={})
+    response = do_request(:get, params)
+    handle_error(response)
+    end
+
+    def put(params={})
+    response = de_request(:put, params)
+    handle_error(response)
+    end
+
+    def post(params={})
+    response = do_request(:post, params)
+    handle_error(response)
+    end
+
+    def handle_error(response)
+      if response.code.to_i >= 500
+        @logger.error "request to #{@uri} failed #{response.code}"
+        raise ServerError
+      end
+      response
+    end
+    
+    def do_request(method, params)
+      # ...
+    end
+  end
+end
+
+# base class bundle up logging errors  with raises exceptions so we need to duplicate the base class error logging code that up as well. 
+# Not good because server error code polices can change. Then updates are required.
+class Myclient < RoflRest::Client
+  def get{params={}}
+    tries = 0 
+    begin 
+      response = do_request(:get, params)
+      return response is response.code == '200'
+      if response.code.to_i >= 500
+        @logger.error "Request to #{@uri} failed #{response.code}"
+      end
+      tries += 1
+      sleep 1
+    end while tries <3
+    raise ServerError, response.code
+  end
+end
