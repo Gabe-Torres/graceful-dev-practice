366a6a68613e0e547c95f74226280dcea5adf2f8
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-05-02 19:21:15 -0500

    An acceptable way.
    not used often as it requires updating more code if class name is ever changed

--- RubyTapas/class090.rb
+++ RubyTapas/class090.rb
@@ -2,12 +2,18 @@ class C
   # class level method - .self
   # self is the object currently being defined, in ruby everything is an object.
   # in ruby everything is an object.
-  def self.foo
-    puts "who needs instances, I'm a class method!"
-  end
+  # def self.foo # were saying attach this new method to the current class object
+  #   puts "who needs instances, I'm a class method!"
+  # end
 
-  puts "Hello from #{self}"
+  # puts "Hello from #{self}"
+
+  #  you can also explicitly call the class instead of self. not used because it is duplication and one more thing to update if the class name is ever changed.
+  def C.bar
+    puts "I'm a class method too!"
+  end
 end
 
-# c.foo
+C.foo
+C.bar
 #>> who needs instances, I'm a class method!
