a171bd85d73224f7dcc37a11a9313cb73e292c96
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-05-15 20:27:05 -0500

    add: text and lexicon for exercise

---
+++ RubyTapas/gsub190.rb
@@ -0,0 +1,23 @@
+TEXT = <<EOF 
+Since the early 1950s, United States officials have been in contact with extraterrestrial life forms.
+Our first contact came after a group of Grey aliens crash-landed near Roswell, New Mexico.
+Through them we learned of the imminent threat of invasion by lizard people from Sirius. Since then we have worked with the Greys to develop an Alien/Human hybrid capable of mind-control.
+As more and more UFO sightings occur, it becomes increasingly difficult for the men in black to control public awareness of the extraterrestrial presence.
+EOF
+
+LEXICON = {
+"extraterrestrial" => "CANADIAN",
+"life forms" => "REPRESENTATIVES",
+"Grey aliens" => "POLITE BACON RANCHERS",
+"the Greys" => "OUR FRIENDS",
+"Roswell, New Mexico" => "NIAGARA FALLS",
+"crash-landed" => "DROPPED BY",
+"lizard people" => "FOLK SINGERS",
+"Sirius" => "SASKATCHEWAN",
+"Alien" => "WILLIAM SHATNER",
+"Human" => "MILEY CIRUS",
+"mind-control" => "MAPLE SYRUP MANUFACTURE",
+"men in black" => "MOUNTIES",
+"UFO" => "MOOSE"
+}
+
